1d99a721f8b41eb9faa6eb1934a5c106f9ee4f31
OpenSourceHistory <historicalatlas@gmx.de>
2015-10-15 10:28:59 +0200

    Still attempting to add property option

--- geojson_writer.py
+++ geojson_writer.py
@@ -6,11 +6,9 @@ import json
 import os
 import sys
 
-
-
 class GeoJSONWriter():
 
-    def __init__(self, query, geojson_path, place_claim="P19"):
+    def __init__(self, query, geojson_path, place_claim="P19", properties=None):
         self.wf = WikiFetcher()
         self.pr = PlaceResolver("places.json")
         self.STEP = 50
